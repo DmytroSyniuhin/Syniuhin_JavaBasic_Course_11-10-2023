e9416315a7f9beaa704732d955e565bd58464390
DmytroSyniuhin <dmytrosyniuhin@gmail.com>
2023-10-11 22:50:23 +0300

    Add new system out println

--- src/HomeWork04.java
+++ src/HomeWork04.java
@@ -1,5 +1,6 @@
 public class HomeWork04 {
     public static void main(String[] args) {
         System.out.println("Hello World and Git! I am Dmytro.");
+        System.out.println("Second commit for homework");
     }
 }
